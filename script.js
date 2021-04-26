99fdd163333ab9f0aafc48ae1e8f351a33517547
quent <quent@LAPTOP-9TSNIHSD>
2021-04-26 17:09:29 +0200

    prise en compte des cas ou le js n'est pas possible --> on affiche rien du menu, et pas un menu vide

--- script.js
+++ script.js
@@ -1,27 +1,31 @@
 
 function on_load_body(){
+
 	/*Récupération des noms des menus*/
-	//const headers = document.querySelectorAll('#div_titre_sommaire > h2'); //permet de récupérer tous les h2 directement enfants des div ayant un id=div_titre_sommaire
 	const headers = document.querySelectorAll('#h2_title_summary');
+	
+	if(headers.length > 0){ //si on a pas reussi a recupérer les titres, on affiche pas de menus
+		var global_div = '<header class="d-flex justify-content-center py-3" ><ul class="nav nav-pills" id="generated_summary" ></ul></header>';
+		document.getElementById('div_generated').innerHTML += global_div;
 
-	for(var i=0, iMax = headers.length; i < iMax; i++) {
+		for(var i=0, iMax = headers.length; i < iMax; i++) {
+		   if(headers[i].textContent.length > 0){ //si le titre récupéré est pas vide
+		   		//Mise en place des noms servant d'ancres pour les jumpto
+		   		var first_word = headers[i].textContent.split(" ")[0].toLowerCase(); //recupere le premier mot de chaque titre, et le met en minuscule
+		   		first_word = first_word.replace(/\s/g,''); //supprime les espaces en trop
+		   		var anchor = "div_"+first_word;
 
-	   //console.log(headers[i].tagName, headers[i].textContent);
+		   	   //Creation des elements li
+			   var li_element = '<li class="nav-item"><a href="#'+anchor+'" class="nav-link" style="font-weight:bold">'+headers[i].textContent+'</a></li>';
 
-	   if(headers[i].textContent.length > 0){
-	   		var first_word = headers[i].textContent.split(" ")[0].toLowerCase(); //recupere le premier mot de chaque titre, et le met en minuscule
-	   		first_word = first_word.replace(/\s/g,''); //supprime les espaces en trop
-	   		var anchor = "div_"+first_word;
-	   		console.log(anchor);
-
-	   	   /*Creation des elements li */
-		   var li_element = '<li class="nav-item"><a href="#'+anchor+'" class="nav-link" style="font-weight:bold">'+headers[i].textContent+'</a></li>';
-
-		   /*Ajout des éléments dans la page*/
-		   document.getElementById('generated_summary').innerHTML += li_element;
+			   //Ajout des éléments dans la page
+			   document.getElementById('generated_summary').innerHTML += li_element;
+			}
 		}
 	}
 }
+
+
 
 
 
